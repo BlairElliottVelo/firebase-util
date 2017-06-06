bcb3ecda5fc59d742a67a790ef4f4323385adff5
Shaun Johansen <shaun.johansen@eyeball.io>
2017-06-06 13:16:02 -0300

    assign firebase-util to module.exports even if firebase is not available
    - enables compatibility with Firebase 3.1+

--- src/expose.js
+++ src/expose.js
@@ -1,5 +1,9 @@
+var firebaseUtil = require('firebase-util');
+
 // place fbutil onto firebase.util namespace
 // if we are in the browser and firebase exists
 if( global.firebase ) {
-  global.firebase.util = require('firebase-util');
+  global.firebase.util = firebaseUtil;
 }
+
+module.exports = firebaseUtil;
